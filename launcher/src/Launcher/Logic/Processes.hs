fff24bae9c0bd58756904a27abf0870f6636f38a
Yura Lazarev <Unisay@users.noreply.github.com>
2023-09-26 21:11:00 +0200

    Tail log files

--- launcher/src/Launcher/Logic/Processes.hs
+++ launcher/src/Launcher/Logic/Processes.hs
@@ -5,22 +5,42 @@ import Control.Concurrent.Supply (freshId)
 import Data.Time (getCurrentTime)
 import Launcher.AppM (AppM, AppState (..), modifyAppStateM)
 import Launcher.Data.Process (Process (..), ProcessId (..))
+import Path (parseRelFile, toFilePath, (</>))
+import Path.IO (getCurrentDir)
+import System.IO
+  ( SeekMode (SeekFromEnd)
+  , hGetChar
+  , hSeek
+  )
+import System.IO.Utf8 (openFile, withFile)
 import System.Process.Typed
   ( getExitCode
   , inherit
   , setStdin
   , setStdout
   , startProcess
+  , useHandleClose
   )
 import System.Process.Typed qualified as System
+import Prelude hiding (withFile)
 
-class Monad m ⇒ ProcessMgmt m where
-  launchProcess ∷ m ProcessId
+class Monad m ⇒ ProcessViewer m where
   listProcesses ∷ m [Process]
+  tailProcessLog ∷ Natural → Process → m [Text]
+
+instance ProcessViewer AppM where
+  listProcesses = gets processes
+  tailProcessLog n Process {processLogFile = (logFilePath, handle)} =
+    liftIO case handle of
+      Nothing → withFile (toFilePath logFilePath) ReadMode (tailFile n)
+      Just hdl → tailFile n hdl
+
+class ProcessViewer m ⇒ ProcessEditor m where
+  launchProcess ∷ m ProcessId
   stopProcess ∷ ProcessId → m ()
   removeExitedProcesses ∷ m ()
 
-instance ProcessMgmt AppM where
+instance ProcessEditor AppM where
   launchProcess ∷ AppM ProcessId
   launchProcess = modifyAppStateM \appState@AppState {..} → do
     let (ProcessId → processId, processIdSupply') = freshId processIdSupply
@@ -32,9 +52,6 @@ instance ProcessMgmt AppM where
           }
       , processId
       )
-
-  listProcesses ∷ AppM [Process]
-  listProcesses = gets processes
 
   stopProcess ∷ ProcessId → AppM ()
   stopProcess processId = modifyAppStateM \appState@AppState {..} → do
@@ -51,19 +68,38 @@ instance ProcessMgmt AppM where
 
 launchProcessIO ∷ ProcessId → IO Process
 launchProcessIO processId = do
+  logFileName ← parseRelFile $ "process-" <> show processId <> ".log"
+  logFilePath ← getCurrentDir <&> (</> logFileName)
+  handle ← openFile (toFilePath logFilePath) ReadWriteMode
+  let stream = useHandleClose handle
   let processConfig =
         System.proc command []
           & setStdin inherit
-          & setStdout inherit
+          & setStdout stream
   processProcess ← startProcess processConfig
   processStarted ← getCurrentTime
-  pure Process {processStopped = False, ..}
+  pure
+    Process
+      { processStopped = False
+      , processLogFile = (logFilePath, Just handle)
+      , ..
+      }
+
+tailFile ∷ Natural → Handle → IO [Text]
+tailFile n hdl = do
+  hSeek hdl SeekFromEnd (-fromIntegral n)
+  lines . toText <$> replicateM (fromIntegral n) (hGetChar hdl)
 
 stopProcessIO ∷ ProcessId → [Process] → IO [Process]
 stopProcessIO processIdToStop processes = do
   forConcurrently processes \process@Process {processId, processProcess} → do
     if processIdToStop == processId
-      then System.stopProcess processProcess $> process {processStopped = True}
+      then
+        System.stopProcess processProcess
+          $> process
+            { processStopped = True
+            , processLogFile = processLogFile process $> Nothing
+            }
       else pure process
 
 removeExitedProcessesIO ∷ [Process] → IO [Process]
@@ -73,7 +109,10 @@ removeExitedProcessesIO processes =
       Nothing → pure process
       Just _c →
         System.stopProcess processProcess
-          $> process {processStopped = True}
+          $> process
+            { processStopped = True
+            , processLogFile = processLogFile process $> Nothing
+            }
 
 command ∷ FilePath
 command = "launcher/loop.sh"
--- launcher/src/Launcher/Shroom/Processes.hs
+++ launcher/src/Launcher/Shroom/Processes.hs
@@ -1,9 +1,29 @@
 module Launcher.Shroom.Processes where
 
+import Data.Foldable (foldrM)
+import Data.Map.Strict qualified as Map
 import Data.Time (defaultTimeLocale, formatTime, rfc822DateFormat)
 import Launcher.Data.Process (Process (..), ProcessId)
-import Launcher.Logic.Processes (ProcessMgmt (..), command)
+import Launcher.Logic.Processes (ProcessEditor (..), ProcessViewer (..), command)
 import Lucid.Extended
+  ( Html_
+  , ToHtml (toHtml)
+  , a_
+  , class_
+  , div_
+  , hxAction_
+  , hxPost_
+  , hxSwap_
+  , hxTarget_
+  , hxTrigger_
+  , ionIcon_
+  , table_
+  , tbody_
+  , td_
+  , th_
+  , thead_
+  , tr_
+  )
 import Shroomz.Component
   ( Component (..)
   , ComponentData (..)
@@ -19,20 +39,57 @@ data Action
   | Refresh
   deriving stock (Show, Read)
 
-type State = [Process]
+type State = Map ProcessId ProcessView
 
-component ∷ ProcessMgmt m ⇒ Component m State Action
+data ProcessView = ProcessView
+  { name ∷ Text
+  , hasStopped ∷ Bool
+  , tailLogLines ∷ [Text]
+  }
+  deriving stock (Show)
+
+component ∷ ∀ m. ProcessEditor m ⇒ Component m State Action
 component = Component {parseAction = parseActionField "action", ..}
  where
   initialise ∷ Applicative m ⇒ m (ComponentData m State)
   initialise = pure ComponentData {children = mempty, userState = mempty}
 
-  update ∷ ProcessMgmt m ⇒ State → Action → m State
-  update _processes action =
-    listProcesses <* case action of
+  update ∷ ProcessEditor m ⇒ State → Action → m State
+  update processViews action = do
+    case action of
       Launch → launchProcess *> removeExitedProcesses
-      Stop processId → stopProcess processId
+      Stop pid → stopProcess pid
       Refresh → removeExitedProcesses
+    processes ← listProcesses
+    foldrM updateViews processViews processes
+   where
+    updateViews ∷ Process → State → m State
+    updateViews process views = do
+      let pid = processId process
+      case Map.lookup pid views of
+        Nothing → updateProcessView pid process views
+        Just oldView →
+          if hasStopped oldView
+            then pure views
+            else updateProcessView pid process views
+
+    updateProcessView ∷ ProcessId → Process → State → m State
+    updateProcessView pid process views = do
+      tailLogLines ← tailProcessLog 60 process
+      pure $
+        views
+          & Map.insert
+            pid
+            ProcessView
+              { name =
+                  toText $
+                    formatTime
+                      defaultTimeLocale
+                      rfc822DateFormat
+                      (processStarted process)
+              , hasStopped = processStopped process
+              , tailLogLines
+              }
 
   render ∷ ComponentPath → State → renderChild → Html_
   render path processes _renderChild = do
@@ -41,7 +98,7 @@ component = Component {parseAction = parseActionField "action", ..}
           : hxSwap_ "outerHTML"
           : hxTarget_ "this"
           : hxAction_ Refresh
-          : [hxTrigger_ "load delay:2s" | not (all processStopped processes)]
+          : [hxTrigger_ "load delay:2s" | not (all hasStopped processes)]
       )
       do
         table_ [class_ "table is-fullwidth is-hoverable"] do
@@ -49,27 +106,26 @@ component = Component {parseAction = parseActionField "action", ..}
             tr_ do
               th_ do "PID"
               th_ do "Started"
-              th_ do "Stopped"
               th_ do "Command"
+              th_ do "Stopped"
+              th_ do "Log"
           tbody_ do
-            forM_ processes \process → tr_ do
-              td_ do show (processId process)
-              td_ do toHtml (processName process)
+            forM_ (Map.toList processes) \(pid, process) → tr_ do
+              td_ do show pid
+              td_ do toHtml (name process)
+              td_ do toHtml command
               td_ do
                 let icon s =
                       ionIcon_
                         (s <> "-circle-outline")
                         (class_ "icon")
-                if processStopped process
+                if hasStopped process
                   then icon "checkmark"
                   else
                     a_
                       [ hxPost_ (toUrlPiece path)
                       , hxTrigger_ "click"
-                      , hxAction_ (Stop (processId process))
+                      , hxAction_ (Stop pid)
                       ]
                       (icon "stop")
-              td_ do toHtml command
-
-processName ∷ Process → String
-processName = formatTime defaultTimeLocale rfc822DateFormat . processStarted
+              td_ do toHtml $ fromMaybe "" $ viaNonEmpty head (tailLogLines process)
